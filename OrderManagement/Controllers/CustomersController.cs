a68f15bf201cd29a47a2abf2a427f4308bffe40a
Jack Heseltine <66922223+heseltime@users.noreply.github.com>
2023-11-11 10:17:05 +0100

    Implement Dependency Injection

---
+++ OrderManagement/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Domain;
+using OrderManagement.Logic;
+
+namespace OrderManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public IOrderManagementLogic Logic { get; set; }
+
+        public CustomersController(IOrderManagementLogic orderManagementLogic)
+        {
+            this.Logic = orderManagementLogic;
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomers()
+        {
+            return await Logic.GetCustomers();
+        }
+    }
+}
--- OrderManagement/Program.cs
+++ OrderManagement/Program.cs
@@ -1,7 +1,12 @@
+using OrderManagement.Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+//builder.Services.AddScoped<IOrderManagementLogic, IOrderManagementLogic>();
+builder.Services.AddSingleton<IOrderManagementLogic, OrderManagementLogic>();
 
 var app = builder.Build();
 
